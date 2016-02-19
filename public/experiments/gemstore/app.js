5f73b15a9a904db6021e7afcaa90750d6eccc191
rohitbegani <rbegani72@gmail.com>
2016-02-19 14:46:33 -0500

    Even more practice

--- public/experiments/gemstore/app.js
+++ public/experiments/gemstore/app.js
@@ -1,1 +1,2 @@
-var app = angular.module('store', [ ]);
+angular
+    .module('store', []);
--- public/experiments/movieapp/js/app.js
+++ public/experiments/movieapp/js/app.js
@@ -1,7 +1,20 @@
-(function(){
-    $(init)
+(function () {
+    $(init);
+
+    var $movieTitleText;
+    var $searchBtn;
 
     function init() {
-        alert("Hello!");
+        //alert("Hello!");
+        $movieTitleText = $('#movieTitleText');
+        $searchBtn = $('#searchBtn');
+
+        $searchBtn.click(searchMovie);
     }
-    })();
+
+    function searchMovie() {
+        var title = $movieTitleText.val();
+
+        alert("Search for " + title);
+    }
+})();
