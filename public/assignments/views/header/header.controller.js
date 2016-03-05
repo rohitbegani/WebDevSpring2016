45dd33e32a29df0ae6a726fc8985617111056863
rohitbegani <rbegani72@gmail.com>
2016-03-05 02:04:25 -0500

    Almost finished

--- public/assignments/views/header/header.controller.js
+++ public/assignments/views/header/header.controller.js
@@ -1,15 +1,17 @@
-(function() {
+(function () {
     "use strict";
     angular
         .module("FormBuilderApp")
         .controller("HeaderController", HeaderController);
+
+    HeaderController.$inject = ['$scope', '$rootScope', '$location'];
 
     function HeaderController($scope, $rootScope, $location) {
         $scope.logout = logout;
         $scope.location = $location;
 
         function logout() {
-            delete $rootScope.currentUser;
+            $rootScope.currentUser = null;
             $location.url("/");
         }
     }
