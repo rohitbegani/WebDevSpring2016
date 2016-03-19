16b0b7b3adf3569eaa760c1585a4ecba678ad1e1
rohitbegani <rbegani72@gmail.com>
2016-03-19 02:35:55 -0400

    Client side requirement done

--- public/assignments/client/services/forms.service.client.js
+++ public/assignments/client/services/forms.service.client.js
@@ -4,58 +4,32 @@
         .module("FormBuilderApp")
         .factory("FormService", FormService);
 
-    function FormService() {
-        var forms = [];
-        forms = [
-            {"_id": "000", "title": "Contacts", "userId": 123},
-            {"_id": "010", "title": "ToDo",     "userId": 123},
-            {"_id": "020", "title": "CDs",      "userId": 234}
-        ];
+    function FormService($http) {
 
         var api = {
-            createFormForUser : createFormForUser,
-            findAllFormsForUser : findAllFormsForUser,
-            deleteFormById : deleteFormById,
-            updateFormById : updateFormById
+            createFormForUser: createFormForUser,
+            findAllFormsForUser: findAllFormsForUser,
+            deleteFormById: deleteFormById,
+            updateFormById: updateFormById
         };
 
         return api;
 
-        function createFormForUser(userId, form, callback) {
-            form._id = (new Date).getTime();
-            form.userId = userId;
-            forms.push(form);
-            return callback(form);
+        function createFormForUser(userId, form) {
+            return $http.post("/api/assignment/user/" + userId + "/form", form);
         }
 
-        function findAllFormsForUser(userId, callback) {
-            var found = [];
-            for (var f in forms) {
-                if (f.userId === userId){
-                    found.push(f);
-                }
-            }
-            return callback(found);
+        function findAllFormsForUser(userId) {
+            return $http.get("/api/assignment/user/" + userId + "/form");
         }
 
-        function deleteFormById(formId, callback) {
-            for (var i=0; i < forms.length; i++){
-                if (forms[i]._id === formId) {
-                    forms.splice(i, 1);
-                    break;
-                }
-            }
-            return callback(forms);
+        function deleteFormById(formId) {
+            return $http.delete("/api/assignment/form/" + formId);
         }
 
-        function updateFormById(formId, newForm, callback) {
-            for (var i=0; i < forms.length; i++){
-                if (forms[i]._id === formId){
-                    newForm._id = formId;
-                    forms[i] = newForm;
-                }
-            }
-            return callback(newForm);
+        function updateFormById(formId, newForm) {
+            return $http.put("/api/assignment/form/" + formId, newForm);
         }
-    }
+
+
 })();
--- public/assignments/client/services/user.service.client.js
+++ public/assignments/client/services/user.service.client.js
@@ -1,26 +1,13 @@
-(function() {
+(function () {
     "use strict";
     angular
         .module("FormBuilderApp")
         .factory("UserService", UserService);
 
-    function UserService() {
-        var users = [];
-        users = [
-            {	"_id":123, "firstName":"Alice",            "lastName":"Wonderland",
-                "username":"alice",  "password":"alice",   "roles": ["student"]		},
-            {	"_id":234, "firstName":"Bob",              "lastName":"Hope",
-                "username":"bob",    "password":"bob",     "roles": ["admin"]		},
-            {	"_id":345, "firstName":"Charlie",          "lastName":"Brown",
-                "username":"charlie","password":"charlie", "roles": ["faculty"]		},
-            {	"_id":456, "firstName":"Dan",              "lastName":"Craig",
-                "username":"dan",    "password":"dan",     "roles": ["faculty", "admin"]},
-            {	"_id":567, "firstName":"Edward",           "lastName":"Norton",
-                "username":"ed",     "password":"ed",      "roles": ["student"]		}
-        ];
-
+    function UserService($http) {
 
         var api = {
+            findUserByUsername: findUserByUsername,
             findUserByCredentials: findUserByCredentials,
             findAllUsers: findAllUsers,
             createUser: createUser,
@@ -28,57 +15,25 @@
             updateUser: updateUser
         };
 
-        return api;
-
-
-        function findUserByCredentials(username, password, callback) {
-            var loggedinUser = null;
-            for (var user in users){
-                if(users[user].username === username && users[user].password === password){
-                    loggedinUser = users[user];
-                }
-                callback(loggedinUser);
-            }
+        function findUserByUsername(username) {
+            return $http.get("/api/assignment/user?username=" + username);
+        }
+        function findUserByCredentials(credentials) {
+            return $http.get("/api/assignment/user?username=" + credentials.username + "&password=" + credentials.password);
+        }
+        function findAllUsers() {
+            return $http.get("/api/assignment/user");
+        }
+        function createUser(user) {
+            return $http.post("/api/assignment/user", user);
         }
 
-        function findAllUsers(callback){
-            callback(users);
+        function deleteUserById(userId) {
+            return $http.delete("/api/assignment/user/" + userId);
         }
-
-        function createUser(user, callback) {
-            var _id = (new Date).getTime();
-            var username = user.username;
-            var password = user.password;
-
-            var newUser = {_id : _id,
-                username : username,
-                password : password
-            };
-
-            users.push(newUser);
-            callback(newUser);
-        }
-
-        function deleteUserById(userId, callback) {
-            for (var user in users) {
-                if (users[user]._id === userId) {
-                    var index = users.indexOf(users[user]);
-                    users.splice(index, 1);
-                }
-            }
-            callback(users);
-        }
-
-        function updateUser(userId, user, callback) {
-            for (var u in users) {
-                if (users[u]._id === userId) {
-                    users[u] = {_id : userId,
-                        firstName : user.firstName, lastName : user.lastName,
-                        username : user.username, password : user.password,
-                        roles : user.roles, email : user.email};
-                }
-            }
-            callback(user);
+        function updateUser(userId, user) {
+            return $http.put("/api/assignment/user/" + userId, user);
         }
     }
+
 })();
